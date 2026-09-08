95f74e84a0f4261efece0ca7d655f8eb76e7b314
Asma Samadi <asmasamadi001@gmail.com>
2026-09-08 12:45:07 +0430

    Solve the url problem

--- travel-genie/client/src/api/axios.js
+++ travel-genie/client/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://travelgenie-backend-fcvw.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://travelgenie-backend-fcvw.onrender.com/api/",
 });
 
 api.interceptors.request.use(
